4b380e719a4103ed004b1a6f695de008675d83fb
mvoshell78 <michael.voshell@yahoo.com>
2014-11-08 20:13:01 -0500

    added array data to tables buy using rows
    added to sections  to the table

---
+++ Voshell_Michael_Project2/build/iphone/build/Voshell_Michael_Project2.build/Debug-iphonesimulator/Voshell_Michael_Project2.build/Objects-normal/i386/TiAction.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiAction.m \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiAction.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBase.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiThreading.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiPublicAPI.h
---
+++ Voshell_Michael_Project2/build/iphone/build/Voshell_Michael_Project2.build/Debug-iphonesimulator/Voshell_Michael_Project2.build/Objects-normal/i386/TiUIiPhoneProxy.d
@@ -0,0 +1,66 @@
+dependencies: \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneProxy.m \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBase.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiThreading.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiPublicAPI.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiEvaluator.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollContext.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/headers/TiCore/TiCore.h \
+  headers/TiCore/Ti.h headers/TiCore/TiBase.h \
+  headers/TiCore/TiContextRef.h headers/TiCore/TiObjectRef.h \
+  headers/TiCore/TiValueRef.h headers/TiCore/WebKitAvailability.h \
+  headers/TiCore/TiStringRef.h headers/TiCore/TiStringRefCF.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/headers/TiCore/TiContextRefPrivate.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollCallback.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollObject.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBindingRunLoop.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUtils.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiDimension.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiColor.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/WebFont.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiFile.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBuffer.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBlob.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiApp.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiHost.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/Bridge.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiStylesheet.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollBridge.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiModule.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/XHRBridge.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiRootViewController.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiControllerProtocols.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiViewProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIView.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiAnimation.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiPoint.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/ListenerEntry.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/LayoutConstraint.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiGradient.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiRect.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiViewTemplate.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneAnimationStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneStatusBarProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneRowAnimationStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneSystemButtonStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneSystemButtonProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneProgressBarStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneActivityIndicatorStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneSystemIconProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneScrollIndicatorStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneTableViewStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneTableViewSeparatorStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneTableViewScrollPositionProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneTableViewCellSelectionStyleProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPadProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPadPopoverProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiViewController.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPadSplitWindowProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiWindowProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiTab.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiTabGroup.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiOSTransitionAnimationProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiOSDocumentViewerProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUIiPhoneAlertDialogStyleProxy.h
