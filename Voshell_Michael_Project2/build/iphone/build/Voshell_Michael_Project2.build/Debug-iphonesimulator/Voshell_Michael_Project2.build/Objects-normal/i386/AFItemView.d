4846e747ce27049265d25631ffa92e7fddaef926
mvoshell78 <michael.voshell@yahoo.com>
2014-11-08 22:48:58 -0500

    json added
    added a json array modified code to work with json

---
+++ Voshell_Michael_Project2/build/iphone/build/Voshell_Michael_Project2.build/Debug-iphonesimulator/Voshell_Michael_Project2.build/Objects-normal/i386/AFItemView.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/AFOpenFlow/AFItemView.m \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/AFOpenFlow/AFItemView.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/AFOpenFlow/AFOpenFlowConstants.h
---
+++ Voshell_Michael_Project2/build/iphone/build/Voshell_Michael_Project2.build/Debug-iphonesimulator/Voshell_Michael_Project2.build/Objects-normal/i386/TiMediaAudioRecorderProxy.d
@@ -0,0 +1,31 @@
+dependencies: \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiMediaAudioRecorderProxy.mm \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiMediaAudioRecorderProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiProxy.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBase.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiThreading.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiPublicAPI.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiEvaluator.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollContext.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/headers/TiCore/TiCore.h \
+  headers/TiCore/Ti.h headers/TiCore/TiBase.h \
+  headers/TiCore/TiContextRef.h headers/TiCore/TiObjectRef.h \
+  headers/TiCore/TiValueRef.h headers/TiCore/WebKitAvailability.h \
+  headers/TiCore/TiStringRef.h headers/TiCore/TiStringRefCF.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/headers/TiCore/TiContextRefPrivate.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollCallback.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/KrollObject.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBindingRunLoop.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/AQRecorder.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/CAStreamBasicDescription.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/CADebugMacros.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/CAXException.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiFile.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiUtils.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiDimension.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiColor.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/WebFont.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBuffer.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiBlob.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiMediaAudioSession.h \
+  /Users/Mich/Desktop/VFW1411/Voshell_Michael_Project2/build/iphone/Classes/TiFilesystemFileProxy.h
