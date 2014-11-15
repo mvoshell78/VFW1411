8cb9041fd9f102e628c9347538b16c2043c548f7
mvoshell78 <michael.voshell@yahoo.com>
2014-11-15 00:40:24 -0500

    bug fixes
    fixed issue with overlapping
    removed images/ path from image title name
    simplified code moved functions the proper places

--- Voshell_Michael_Project3/Resources/app.js
+++ Voshell_Michael_Project3/Resources/app.js
@@ -6,7 +6,7 @@ var imageFiles = imagesFolder.getDirectoryListing();
 var pageWidth = Ti.Platform.displayCaps.platformWidth;
 var pageHeight = Ti.Platform.displayCaps.platformHeight;
 var boxCount = 4;
-var margin = pageWidth * .04; // multipling .07 makes margin relitive to device size
+var margin = pageWidth * .01; // multipling .07 makes margin relitive to device size
 var boxWidth =  pageWidth - ((boxCount + 1) * margin);
 var boxSize = boxWidth/ boxCount;
 
@@ -44,9 +44,6 @@ var container = Ti.UI.createScrollView({
 	top: 0
 });
 
-var winThree = Ti.UI.createWindow({
-	backgroundColor:"#E0E6F5"
-});
 
 
 var back = Ti.UI.createLabel({
@@ -66,14 +63,10 @@ var back2 = Ti.UI.createLabel({
 var closeWindowTwo = function(){
 	winTwo.close();
 };
-var closeWindowThree = function(){
-	winThree.close();
-};
-var galleryOpen = function(){
-	//winOne.close();
-	
 
- 	for (i=0; i< imageFiles.length; i++){	
+var galleryOpen = function(){
+	
+	for (i=0; i< imageFiles.length; i++){	
 		var newView = Ti.UI.createView({
 			backgroundColor: "#E0F2F5",
 			borderWidth: 1,
@@ -83,42 +76,58 @@ var galleryOpen = function(){
 			top: margin,
 			left: margin,
 			});
-	var thumbnail = Ti.UI.createImageView({
-		image: "images/" + imageFiles[i], 
-		// used to expand images to fit entire thumbnail
-		top: 0,
-		height: newView.height*2,
-		width: newView.width*2
-		});
+			
+		var thumbnail = Ti.UI.createImageView({
+			image: "images/" + imageFiles[i], 
+			// used to expand images to fit entire thumbnail
+			top: 0,
+			height: newView.height*2,
+			width: newView.width*2
+			});
+			
 	newView.add(thumbnail);
 	container.add(newView);
 	};
-		
-	// event propagation ~ taking an event listener and adding it to the parent object
-	// one listener to simplify code
-	container.addEventListener("click",function(event){
-	
-			var fullImage = Ti.UI.createImageView({
-				image: event.source.image
-				});
-			var fullImageTxt = Ti.UI.createLabel({
-				text: event.source.image, 
-				top: 20,
-				font: {fontSize: 15, fontFamily: "ArialRounded"}
-				
-			});
-				winThree.add(fullImage, fullImageTxt, back2);
-				winThree.open();
-		});
-		
-		winTwo.add(back, border, container);
+	winTwo.add(back, border, container);
 		winTwo.open();	
 };
+	
 
+var fullWindow = function(imageSource){
+	
+	var winThree = Ti.UI.createWindow({
+	backgroundColor:"#E0E6F5"
+	});
 
+			var fullImage = Ti.UI.createImageView({
+				image: imageSource,
+				
+				});
+			var picTitle = (imageSource.substr(7));
+			var fullImageTxt = Ti.UI.createLabel({
+				text: picTitle,
+				top: 40,
+				font: {fontSize: 15, fontFamily: "ArialRounded"}
+				});
+				
+				winThree.add(fullImage, fullImageTxt, back2);
+				winThree.open();
+
+	var closeWindowThree = function(){
+	winThree.close();
+	};	
+		back2.addEventListener("click",closeWindowThree);
+	};
+
+	
 	winOne.add(openView);
 	openView.add(openTxt);
 	openView.addEventListener("click", galleryOpen);
+	container.addEventListener("click",function(event){
+		console.log(event.source);
+		if(event.source = "[object TiUIImageView]"){
+			fullWindow(event.source.image);
+			};
+		});
 	back.addEventListener("click",closeWindowTwo);
-	back2.addEventListener("click",closeWindowThree);
 	winOne.open(); 
